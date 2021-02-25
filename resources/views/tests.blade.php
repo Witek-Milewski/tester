d9b56455bc5ac2bf809c87921bb555000934f7b8
Witek-Milewski <milewskiwitek8@gmail.com>
2021-02-25 21:40:32 +0100

    Update Tests

--- resources/views/tests.blade.php
+++ resources/views/tests.blade.php
@@ -1,15 +1,173 @@
-@foreach ($tests as $test)
-    {{$test->userid}}
-@endforeach
+<!DOCTYPE html>
+<html lang="pl">
 
-<form method="POST">
-    <input name="title" type="text" placeholder="Tytuł">
-    <input name="question" type="text" placeholder="Pytanie">
-    <input name="a" type="text" placeholder="Odpowiedz A">
-    <input name="b" type="text" placeholder="Odpowiedz B">
-    <input name="c" type="text" placeholder="Odpowiedz C">
-    <input name="d" type="text" placeholder="Odpowiedz D">
-    <input name="answer" type="text" placeholder="Poprawna">
-    <input type="submit" value="Dodaj">
-    @csrf
-</form>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tester - Testy</title>
+    <link rel="stylesheet" href="css/tests.css">
+</head>
+
+<body>
+    
+    <header>
+        <p>Twoje testy</p>
+        <div class="add">
+            <a href="tests/add"><button>Stwórz nowy</button></a>
+        
+        </div>
+    </header>
+
+    <main>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="active">Aktywny</div>
+        </article>
+
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="active">Aktywny</div>
+        </article>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+        <article>
+            <div class="date">
+                Data utworzenia: 25.02.2021
+            </div>
+            <div class="title">
+                Pierwiastki układu okresowego
+            </div>
+            <div class="desctription">
+                Sprawdzian ze znajomości układu okresowego, atomów oraz protonów.
+            </div>
+            <div class="noactive">Nieaktywny</div>
+        </article>
+
+    </main>
+</body>
+
+</html>
